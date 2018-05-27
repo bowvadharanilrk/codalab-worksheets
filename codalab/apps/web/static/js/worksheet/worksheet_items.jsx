7ff6fcab1b46a0ea708dc5c1ff3a647d5e782941
Dennis Jeong <wonjeo@gmail.com>
2018-05-27 01:19:39 -0700

    [WIP] converted to use BlockSchemas for markup

--- codalab/apps/web/static/js/worksheet/worksheet_items.jsx
+++ codalab/apps/web/static/js/worksheet/worksheet_items.jsx
@@ -162,7 +162,7 @@ var addWorksheetItems = function(props, worksheet_items) {
     props.url = url;
 
     var constructor = {
-      'markup': MarkdownItem,
+      'markup_block': MarkdownItem,
       'table': TableItem,
       'contents': ContentsItem,
       'worksheet': WorksheetItem,
